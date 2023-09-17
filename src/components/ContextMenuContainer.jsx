15a55142705e69ddf2b4bc1ebc94550566ac91c2
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-17 16:54:50 -0700

    fixing mobile hold touch experience

--- src/components/ContextMenuContainer.jsx
+++ src/components/ContextMenuContainer.jsx
@@ -45,7 +45,7 @@ const ContextMenuContainer = ({ userID, itemId, handleItemDelete, groupID, child
   const handleTouchStart = (e) => {
     longPressTimer.current = setTimeout(() => {
       handleContextMenu(e, menuItems);
-    }, 500);
+    }, 550);
   };
 
   const handleTouchEnd = () => {
