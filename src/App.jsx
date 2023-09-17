bbd25978e33347935d8b3a42b6a762a2159c3d6f
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-16 23:57:29 -0700

    implmementing entire change of login system

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Section from "./components/Section/Section"
+import Section from "./components/Section/Section";
 import axios from 'axios';
 import { useEffect, useState } from "react";
 import TaskSection from "./components/TaskSection";
@@ -10,7 +10,6 @@ import notificationSoundSrc from "./assets/sound2.mp3"
 import "./App.css";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GoogleLogin } from '@react-oauth/google';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -25,111 +24,96 @@ export default function App() {
     toast.success("Task Complete!");
   }
 
-  const [userData, setUserData] = useState({});  // Changed to an object
+  const [userData, setUserData] = useState({});
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [userID, setUserID] = useState(null);
+  const [token, setToken] = useState(null);
+  const [googleUserId, setGoogleUserId] = useState(null); // This will store the user's Google ID
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
-  console.log("userData in App.jsx: ", userData)
+  const fetchData = () => {
+    if (!token) return;
 
-  const fetchData = (id) => {
-    axios.get(`${apiUrl}/${id}`)
-      .then((response) => {
-        console.log(response.data);
-        setUserData(response.data);
-      }).catch(error => {
-        console.error('Error fetching user data:', error);
-      });
+    axios.get(`${apiUrl}/${googleUserId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      console.log(response.data);
+      setUserData(response.data);
+    }).catch(error => {
+      console.error('Error fetching user data:', error);
+    });
   };
 
   const responseGoogle = (response) => {
     console.log("response: ", response);
-
-    // Here, you can check if the response contains an error or if it's successful
-    // For successful responses, you typically want to save the token and user details
-    if (response?.tokenId) {
-        // handle successful login
+    if (response?.tokenId && response?.profileObj?.googleId) {
+        setToken(response.tokenId);
+        setGoogleUserId(response.profileObj.googleId); // Set the Google ID for the user
+        localStorage.setItem('userToken', response.tokenId);
     } else {
         // handle errors or failed login
     }
-};
-
+  };
 
   useEffect(() => {
-    const storedUserID = localStorage.getItem('userID');
-
-    if (!storedUserID) {
-        const userInputID = prompt("Please enter your unique user ID:");
-        if (userInputID && userInputID.trim().length > 0) {
-            localStorage.setItem('userID', userInputID);
-            setUserID(userInputID); // Set userID state
-        } else {
-            console.error('Valid User ID is required.');
-        }
-    } else {
-        setUserID(storedUserID); // Set userID state
+    const storedToken = localStorage.getItem('userToken');
+    if (storedToken) {
+      setToken(storedToken);
     }
-}, []);
+  }, []);
 
-useEffect(() => {
-    if (userID) {
-        fetchData(userID); // Fetch data whenever userID changes
+  useEffect(() => {
+    if (googleUserId && token) {
+      fetchData();
     }
-}, [userID]);
-
+  }, [googleUserId, token]);
 
   return (
     <GoogleOAuthProvider clientId="307941107777-dch3oqprahp6b0l8ea21aiquilkq7suo.apps.googleusercontent.com">
-    <div className="select-none md:pt-8 flex flex-col md:flex-row min-h-screen w-screen" onContextMenu={(e) => {
-      e.preventDefault();
-    }}>
-      <ToastContainer
-        position="top-center"
-        autoClose={500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-
-      <motion.img id="SideBar" 
-        initial={{ scale: 1 }} 
-        whileHover={{ scale: 1.1 }}
-        className="fixed right-5 top-5 w-7" src={menuIcon} alt="side menu" onClick={toggleSidebar}
-      />
-
-      { sidebarOpen && (
-        <SideBar toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen}/>
-      )}
-
-      <div className="w-full md:w-1/2 md:ml-24">
-        {userID && <TaskSection userID={userID} taskCompleteNotify={taskCompleteNotify} sectionTitle="Action Items" />}
+      <div className="select-none md:pt-8 flex flex-col md:flex-row min-h-screen w-screen" onContextMenu={(e) => {
+        e.preventDefault();
+      }}>
+        <ToastContainer
+          position="top-center"
+          autoClose={500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <motion.img id="SideBar" 
+          initial={{ scale: 1 }} 
+          whileHover={{ scale: 1.1 }}
+          className="fixed right-5 top-5 w-7" src={menuIcon} alt="side menu" onClick={toggleSidebar}
+        />
+        {sidebarOpen && (
+          <SideBar toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen}/>
+        )}
+        <div className="w-full md:w-1/2 md:ml-24">
+          {googleUserId && <TaskSection userID={googleUserId} taskCompleteNotify={taskCompleteNotify} sectionTitle="Action Items" />}
+        </div>
+        <div className="w-full md:w-1/2 mb-8 md:ml-2 mr-8 pl-3 pt-2 pb-2">
+          <Section userData={userData} userID={googleUserId} setUserData={setUserData} sectionTitle="Goals & Objectives" sectionType="goals" sectionItems={userData.goals} sectionDetails={userData.goaldetails} />
+          <Section userData={userData} userID={googleUserId} setUserData={setUserData} sectionTitle="Challenges & Obstacles" sectionType="challenges" sectionItems={userData.challenges} sectionDetails={userData.challengedetails} />
+        </div>
+        {!token && (
+          <GoogleLogin
+            clientId="307941107777-dch3oqprahp6b0l8ea21aiquilkq7suo.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            onSuccess={responseGoogle}
+            onError={responseGoogle}
+          />
+        )}
       </div>
-
-      <div className="w-full md:w-1/2 mb-8 md:ml-2 mr-8 pl-3 pt-2 pb-2">
-        <Section userData={userData} userID={userID} setUserData={setUserData} sectionTitle="Goals & Objectives" sectionType="goals" sectionItems={userData.goals} sectionDetails={userData.goaldetails} />
-        
-        <Section userData={userData} userID={userID} setUserData={setUserData} sectionTitle="Challenges & Obstacles" sectionType="challenges" sectionItems={userData.challenges} sectionDetails={userData.challengedetails} />
-        {/* <Section sectionTitle="Solutions" /> */}
-        {/* <Section userID={userID} setUserData={setUserData} sectionTitle="Inspiration" sectionType="inspiration" sectionItems={userData.inspiration} sectionDetails={userData.inspirationdetails} /> */}
-        {/* <Section userID={userID} setUserData={setUserData} sectionTitle="Insights & Ideas" sectionType="insightsIdeas" sectionItems={userData.insightsIdeas} sectionDetails={userData.insightIdeasdetails}/> */}
-      </div>
-
-      <GoogleLogin
-      clientId="307941107777-dch3oqprahp6b0l8ea21aiquilkq7suo.apps.googleusercontent.com"
-      buttonText="Login with Google"
-      onSuccess={responseGoogle}
-      onError={responseGoogle}
-/>
-      
-    </div>
     </GoogleOAuthProvider>
   )
 }
