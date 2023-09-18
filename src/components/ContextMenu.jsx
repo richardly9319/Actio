e95b47010bec1db1e7badefb71f89d291464aff4
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-17 18:57:27 -0700

    a

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -4,42 +4,26 @@ const ContextMenu = ({ setContextMenuIsVisible, items, onClose, onClick }) => {
   const menuRef = useRef(null);
 
   useEffect(() => {
-    let touchEventFired = false;
-
     const handleClickOutside = (event) => {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
-        event.stopPropagation();
         onClose();
         setContextMenuIsVisible(false);
       }
     };
 
-    const handleTouchStart = (event) => {
-      event.preventDefault();
-      touchEventFired = true;
-      handleClickOutside(event);
-    };
-
-    const handleMouseDown = (event) => {
-      if (!touchEventFired) {
-        handleClickOutside(event);
-      }
-      touchEventFired = false;
-    };
-
     // Attach the event listeners
-    document.addEventListener('touchstart', handleTouchStart, { passive: false} );
-    document.addEventListener('mousedown', handleMouseDown, { passive: false});
+    document.addEventListener('touchstart', handleClickOutside, { passive: true });
+    document.addEventListener('mousedown', handleClickOutside, { passive: true });
 
     // Clean up the event listeners on component unmount
     return () => {
-      document.removeEventListener('touchstart', handleTouchStart);
-      document.removeEventListener('mousedown', handleMouseDown);
+      document.removeEventListener('touchstart', handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [onClose]);
 
-
-  const handleItemClick = (action) => {
+  const handleItemClick = (e, action) => {
+    e.stopPropagation(); // To ensure the click doesn't propagate up and trigger handleClickOutside
     onClick(action);
     onClose();
   };
@@ -50,10 +34,7 @@ const ContextMenu = ({ setContextMenuIsVisible, items, onClose, onClick }) => {
         <li
           key={index}
           className="px-4 py-2 hover:bg-gray-100"
-          onClick={(e) => {
-            handleItemClick(item.action);
-            e.stopPropagation();
-          }}
+          onClick={(e) => handleItemClick(e, item.action)}
         >
           {item.label}
         </li>
